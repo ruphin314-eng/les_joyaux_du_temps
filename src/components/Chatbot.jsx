a4ff81378a36e7449feb9e89d3371058ea2f2fed
ruphin314-eng <ruphinruphin314@gmail.com>
2025-10-31 23:25:15 +0100

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -25,7 +25,7 @@ const Chatbot = () => {
     setTyping(true);
 
     try {
-      const res = await fetch("http://localhost:5000/chat", {
+      const res = await fetch("https://server-1-wsc6.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
